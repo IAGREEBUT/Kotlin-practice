b9367e97d3bf42ff47406d2f2dfe88c5f2410fa3
lyj <dldbwjd24@gmail.com>
2024-01-18 23:56:02 +0900

    1. Hello kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,20 @@
 fun main(args: Array<String>) {
-    println("Hello World! - test")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    //상수 : 재할당 불가
+    val pi : Double = 3.14
+    println(pi)
+
+    //variable
+    var age : Int = 39
+    println(age) //39
+    age = 45
+    println(age) //45 -> 원하는 값으로 변경(재할당) 가능
+
+
+    //why 자료형?
+    /*
+    메모리를 효율적으로 사용하기 위해서 사용함
+    변수에 알맞은 크기의 메모리를 할당하기 위해서
+     */
+
 }
