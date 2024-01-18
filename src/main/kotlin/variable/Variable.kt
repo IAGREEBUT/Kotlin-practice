a50019224bee7ba9d3eb6048f6022f8a470128f9
lyj <dldbwjd24@gmail.com>
2024-01-18 23:56:29 +0900

    2. Variable and const

---
+++ src/main/kotlin/variable/Variable.kt
@@ -0,0 +1,22 @@
+package variable
+
+fun main(args: Array<String>) {
+
+    //상수 : 재할당 불가
+    val pi : Double = 3.14
+    println(pi)
+
+    //variable
+    var age : Int = 39
+    println(age) //39
+    age = 45
+    println(age) //45 -> 원하는 값으로 변경(재할당) 가능
+
+
+    //why 자료형?
+    /*
+    메모리를 효율적으로 사용하기 위해서 사용함
+    변수에 알맞은 크기의 메모리를 할당하기 위해서
+     */
+
+}
