90a12a132539f117da14a6bb3c83e8622e692209
lyj <dldbwjd24@gmail.com>
2024-01-18 23:56:38 +0900

    3. data type

---
+++ src/main/kotlin/datatype/DataTypes.kt
@@ -0,0 +1,55 @@
+package datatype
+
+/*
+
+Data Type
+
+
+1. Numbers
+
+//자료형 추론
+: 자료형 을 써주지 않아도 자동으로 코틀린이 추론함
+
+1) Byte //자료형 생략 불가
+var mByte : Byte = 1
+
+2) Short
+var mShort : Short = 1000  //자료형을 생략하면 자료형 추론이 Int로 추론됨
+
+3) Int
+var mInt : Int = 1000
+
+4) Long
+var mLong : Long = 10000000000L
+
+5) Float : 소수점 6-7째 자리까지 표현 가능
+var mFloat : Float = 1.123f
+
+6) Double : 소수점 16째 자리까지 표현 가능
+var mDouble : Double = 1.234567
+
+
+
+2. Characters
+
+var mChar : Char = 'a'
+
+
+3. Strings
+var mString : String = "Hello kotlin"
+
+
+4. Booleans
+var mFalse: Boolean = false
+var mTrue : Boolean = true
+
+
+4. Arrays
+
+var mArray : Array<String> = arrayOf("Kotlin","is","fun)
+mArray[0] or mArray.get(0) // Kotlin
+mArray[1] // is
+mArray[2] // fun
+
+
+ */
